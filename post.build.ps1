9b04bd5c83934b5ffd51a4e46b90ad830625907d
Lex Li <support@lextm.com>
2022-12-27 01:17:44 -0500

    Added test steps.

--- post.build.ps1
+++ post.build.ps1
@@ -1,2 +1,7 @@
 cd ./src/dotnet-ildasm
 dotnet pack -c Release -o ../../nupkg
+cd ../..
+
+dotnet tool uninstall --global dotnet-ildasm2 
+dotnet tool install --global --add-source ./nupkg dotnet-ildasm2
+ildasm --version
